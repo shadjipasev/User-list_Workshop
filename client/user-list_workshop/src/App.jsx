5bf9b122f1b69d2b0e33b75691b71e5d0ac7f448
Spasidar Hadzhipase <shadjipasev@gmail.com>
2023-11-08 13:58:07 +0200

    Baseline initialization

---
+++ client/user-list_workshop/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
